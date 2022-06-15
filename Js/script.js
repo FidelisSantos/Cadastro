a87394fba4c789b2e1d73be31aacaad57424e962
FidelisSantos <vitorfidelissantos@gmail.com>
2022-06-15 01:36:19 -0300

    Usuário padrão e atualização

--- Js/script.js
+++ Js/script.js
@@ -1,27 +1,75 @@
 const showDatabase = document.getElementsByClassName("card");
 const showAllInformationPeople = document.getElementsByClassName("modal-body");
 const showCardName = document.getElementsByClassName("modal fade");
-var columns = {valueNames: ['url','name','CPF','surName','genre','phone','state', 'district', 'adress', 'numberAdress', 'adressComplement', 'obs','up']};
-var values = [ {url: "https://scontent-gru1-1.xx.fbcdn.net/v/t1.6435-9/205308418_4263790420326536_4449074302394512871_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeE9nKukhHqDwYxE_mgf_4zQ7S_jBHLAtkbtL-MEcsC2Rrh4W3mNIyoo9fNECQWE-_85I0-rd54kO9GMrriX9CIv&_nc_ohc=9KEzKu21YkYAX8kZ7c_&_nc_ht=scontent-gru1-1.xx&oh=00_AT_btdPscMIseFxTbD3YSvfLa66IcDZQB4e-toGHtnVJpQ&oe=62CFEB97",
-                name:"Vitor Hugo Fidelis dos Santos",
-                CPF: "12312312345",
-                surName: "Fidelis",
-                genre: "M",
-                phone: "41998441966",
-                state: "PR",
-                district:
 
+const People = [];
+
+window.onload = function() {
+    addDefaultPerson();
 }
-];
-document.getElementsByClassName("modal fade").textContent=person.name;
-function insertItem(item, index) {
-    let tr = document.createElement('tr')
+
+function addDefaultPerson() {
+    People.push(
+        {
+            id: People.length + 1,
+            url: "https://scontent-gru1-1.xx.fbcdn.net/v/t1.6435-9/205308418_4263790420326536_4449074302394512871_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeE9nKukhHqDwYxE_mgf_4zQ7S_jBHLAtkbtL-MEcsC2Rrh4W3mNIyoo9fNECQWE-_85I0-rd54kO9GMrriX9CIv&_nc_ohc=9KEzKu21YkYAX8kZ7c_&_nc_ht=scontent-gru1-1.xx&oh=00_AT_btdPscMIseFxTbD3YSvfLa66IcDZQB4e-toGHtnVJpQ&oe=62CFEB97",
+            name:"Vitor Hugo Fidelis dos Santos",
+            CPF: "12312312345",
+            surName: "Fidelis",
+            genre: "M",
+            phone: "41998441966",
+            state: "PR",
+            district: "Boqueirão",
+            adress: "Rua Cascavel",
+            numberAdress: "1715",
+            adressComplement: "Casa 05",
+            obs:"Testando",
+            updateddate:"",
+            createdate:new Date()
+        });
+
+    People.forEach(populateCards);
+}
+function populateCards(person){
+
+    let card =`<div class="card" style="width: 18rem;">
+    <img class="card-img-top" src="`+person.url+`">
+    <div class="card-body">
+      <h5 class="card-title">`+person.name+`</h5>
+       <button type="button" class="btn btn-link" data-toggle="modal" data-target="#exampleModal" onclick="updateModal(`+person.id+`)">
+            Veja mais
+        </button>
+    </div>
+  </div>`
   
-    tr.innerHTML = `
-      <td>${person.name}</td>
-      <td>${person.CPF}</td>
-      <td>${item.salario}</td>
-      <td class="acao">
+    document.getElementById("people").innerHTML = card;
+}
+
+function updateModal(personID){
+    var person = People[personID -1]
+    document.getElementById("personID").value = personID;
+    document.getElementById("modalPersonName").value = person.name;
+    document.getElementById("modalPersonId").value = person.id;
+}
+
+function editPerson()
+{
+    let id = document.getElementById("personID").value;
+    let person = People[id];
+
+    person.name = document.getElementById("personName").value;
+    person.cpf = document.getElementById("personCPF").value;
+}
+
+  function insertItems() {
+
+    var person = getPerson();
+
+    let tr = `
+    <tr>
+    <td>` + person.name +`</td>
+    <td>` + person.cpf +`</td>
+     </tr> 
     `
-    tbody.appendChild(tr)
+    document.getElementById("tabela").innerHTML = tr
   }
